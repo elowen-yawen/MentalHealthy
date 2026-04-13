33a2233bbe829af335e968633dad124531a33e0a
Elowen <3376027149@qq.com>
2026-04-13 21:19:11 +0800

    第一次提交：环境配置完成，加入分流模型基础代码

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld />
+  <!-- <div>
+    <el-button type="primary">Element Plus 按钮</el-button>
+    <router-link to="/home">Home</router-link>
+  </div> -->
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+// import { createRouter, createWebHashHistory } from 'vue-router'
+// import Home from '@/components/Home.vue'
+// const backsroutes=[
+//     {
+//         path:'/home',
+//         component:Home
+//     }
+// ]
+// const router=createRouter({
+//     history:createWebHashHistory(),
+//     routes:backsroutes
+// })
+// App.use(router)

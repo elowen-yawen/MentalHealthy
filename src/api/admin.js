b5e53ae8b868986f7b461973b2de916dc67be889
Elowen <3376027149@qq.com>
2026-04-25 10:22:24 +0800

    完成了图片传输

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,4 +1,5 @@
 import service from "@/utils/request";
+import { Header } from "element-plus/es/components/table-v2/src/components/index.mjs";
 export function login(data){
     return service.post('/user/login',data)
 }
@@ -7,4 +8,17 @@ export function categoryTree(){
 }
 export function articlePage(params){
     return service.get('/knowledge/article/page',{params})
+}
+export function handleImageHttpRequest(file,businessInfo){
+   //formData用于文件传输,一般传输图片音频等，需要设置headers: { 'Content-Type': 'multipart/form-data' }。
+    const formdata=new FormData()
+    formdata.append('file',file)
+    formdata.append('businessType','ARTICLE')
+    formdata.append('businessId',businessInfo.businessId)
+    formdata.append('businessField','cover')
+    return service.post('/file/upload',params,{
+        headers:{
+            'Content-Type':'multipart/form-data'
+        }
+    })
 }
--- src/components/ArticleDialog.vue
+++ src/components/ArticleDialog.vue
@@ -2,54 +2,73 @@
     <el-dialog v-model="prop.dialogVisible" title="新增文章" width="500" @close="handleClose">
         <el-form>
             <el-form-item label="文章标题">
-                <el-input v-model="formData.articlName" />
+                <el-input v-model="formData.title" />
             </el-form-item>
             <el-form-item label="所属分类">
-                <el-select v-model="formData.category" placeholder="请选择分类">
+                <el-select v-model="formData.categoryId" placeholder="请选择分类">
                     <el-option v-for="p in prop.categories" :key="p" :value="p.label">{{ p.label }}</el-option>
                 </el-select>
             </el-form-item>
             <el-form-item label="文章摘要">
-                <el-input v-model="formData.define" placeholder="请输入文章摘要（可选）" type="textarea" max="1000" rows="4" />
+                <el-input v-model="formData.summary" placeholder="请输入文章摘要（可选）" type="textarea" max="1000" rows="4" />
+            </el-form-item>
+             <el-form-item label=" 标签 ">
+                <el-select v-model="formData.tags" placeholder="请选择标签" multiple filterable allow-create style="padding-left: 25px;">
+                    <el-option v-for="p in commonTags" :key="p" :value="p" :label="p">{{ p }}</el-option>
+                </el-select>
             </el-form-item>
             <el-form-item label="封面图片">
-                <el-upload v-model="formData.uploadImg" class="upload-demo"
+                <el-upload v-model="formData.coverImage" class="upload-demo"
                     action="#" multiple
                     :before-upload="beforeUpload"
                     :http-request="handleHttpUpload"
+                    :show-file-list="false"
                     accept="images/*">
                     <div class="cover-img" v-if="!imgUrl">点击上传图片</div>
-                    <img :src="imgUrl" alt="" v-else>
+                    <img :src="imgUrl" alt="" v-else style="" class="cover-img">
+                    <div class="cover-img" v-if="!imgUrl">
+                        <el-button type="danger" @click="handleRemoveImage">
+                            点击移除列表
+                        </el-button>
+                    </div>
                 </el-upload>
             </el-form-item>
         </el-form>
     </el-dialog>
 </template>
 <script setup>
-import { ref, reactive, computed ,updated} from 'vue';
+import { ref, reactive, computed } from 'vue';
 import { ElMessage } from 'element-plus';
-//关于elmessage的使用
 const prop = defineProps(['dialogVisible', 'categories'])
 const emit = defineEmits(['update:visiblity'])
+const commonTags = [
+  '情绪管理', '焦虑', '抑郁', '压力', '睡眠', 
+  '冥想', '正念', '放松', '心理健康', '自我成长',
+  '人际关系', '工作压力', '学习方法', '生活技巧'
+]
 const handleClose = () => {
     emit('update:visiblity', false)
 }
 const formData = reactive({})
 const imgUrl=ref()
 const beforeUpload=(file)=>{
+    console.log(file)
     const isImage=file.type.startsWith('image/')
     const isLt5M=file/1024/1024<5
     if(!isImage){
         ElMessage.error("上传封面图片，请选择图片文件")
         return false
     }
-    if(!isLt5M){
-        ElMessage.error("上传的文件应少于5M")
-        return false
-    }
+    // if(!isLt5M){
+    //     ElMessage.error("上传的文件应少于5M")
+    //     return false
+    // }
     return true
 }
-const handleHttpUpload=()=>{
+const handleHttpUpload=async({file})=>{
+    
+}
+const handleRemoveImage=()=>{
 
 }
 </script>

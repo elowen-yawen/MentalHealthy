805eac001021e54da8429d64d70700637f15ca6e
Elowen <3376027149@qq.com>
2026-04-25 15:53:38 +0800

    忘记写remedy了

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,24 +1,27 @@
 import service from "@/utils/request";
 import { Header } from "element-plus/es/components/table-v2/src/components/index.mjs";
-export function login(data){
-    return service.post('/user/login',data)
+export function login(data) {
+    return service.post('/user/login', data)
 }
-export function categoryTree(){
+export function categoryTree() {
     return service.get('/knowledge/category/tree')
 }
-export function articlePage(params){
-    return service.get('/knowledge/article/page',{params})
+export function articlePage(params) {
+    return service.get('/knowledge/article/page', { params })
 }
-export function handleImageHttpRequest(file,businessInfo){
-   //formData用于文件传输,一般传输图片音频等，需要设置headers: { 'Content-Type': 'multipart/form-data' }。
-    const formdata=new FormData()
-    formdata.append('file',file)
-    formdata.append('businessType','ARTICLE')
-    formdata.append('businessId',businessInfo.businessId)
-    formdata.append('businessField','cover')
-    return service.post('/file/upload',params,{
-        headers:{
-            'Content-Type':'multipart/form-data'
+export function handleImageHttpRequest(file, businessInfo) {
+    //formData用于文件传输,一般传输图片音频等，需要设置headers: { 'Content-Type': 'multipart/form-data' }。
+    const formdata = new FormData()
+    formdata.append('file', file)
+    formdata.append('businessType', 'ARTICLE')
+    formdata.append('businessId', businessInfo.businessId)
+    formdata.append('businessField', 'cover')
+    return service.post('/file/upload', formdata, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
         }
     })
+}
+export function handleNewArticle(formData) {
+    return service.post('/knowledge/article', formData)
 }
